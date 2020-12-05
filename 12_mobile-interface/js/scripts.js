667c10f7271755d53d303e7684a2d4ccbf6105f0
Talon Durrant <talonmdurrant@gmail.com>
2020-12-05 16:23:35 -0700

    finished module 12 mobile interface

--- 12_mobile-interface/js/scripts.js
+++ 12_mobile-interface/js/scripts.js
@@ -8,6 +8,40 @@ fetch(jsonData)
     //Once it comes back, display it to the console.
     console.log(data)
 
-    let imageTag = document.createElement("img")
-    imageTag.src = "images/seranat"
+    data.forEach((hotel) => {
+      let section = document.createElement("section")
+
+      let image = document.createElement("img")
+      let caption = document.createElement("figcaption")
+      let figure = document.createElement("figure")
+
+      image.src = hotel.photo
+      caption.innerText = hotel.name
+
+      figure.appendChild(image)
+      figure.appendChild(caption)
+
+      let div = document.createElement("div")
+
+      let address = document.createElement("span")
+      address.innerHTML = `
+      <i class="icon ion-md-car"></i>
+      <p>${hotel.address[0]}</p>
+      <p>${hotel.address[1]}</p>
+      `
+
+      let phone = document.createElement("span")
+      phone.innerHTML = `
+      <i class="icon ion-md-call"></i>
+      <p>${hotel.phone}</p>
+      `
+
+      div.appendChild(address)
+      div.appendChild(phone)
+
+      section.appendChild(figure)
+      section.appendChild(div)
+
+      document.getElementById("hotels-div").appendChild(section)
+    })
   }) //end of "then" fat arrow function
